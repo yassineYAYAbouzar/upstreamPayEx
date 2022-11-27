2e7f57517eebb18501e07a43eec1d832741e4fd5
Floki <anna@MacBook-Pro-van-anna.local>
2022-11-27 20:44:23 +0100

    init project

---
+++ src/main/java/com/upstream/py/ex/UpstreamPyExApplication.java
@@ -0,0 +1,13 @@
+package com.upstream.py.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpstreamPyExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpstreamPyExApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/upstream/py/ex/UpstreamPyExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upstream.py.ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpstreamPyExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
